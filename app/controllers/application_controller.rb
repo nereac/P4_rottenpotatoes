358e148b922b44f6045255366c36c6565b7344ab
nereac <n.corteslomana@gmail.com>
2012-11-13 19:45:33 +0100

    Apartado 6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :set_current_user, :except => :login
+  before_filter :set_current_user, :except => [:login]
   protected # prevents method from being invoked by a route
   def set_current_user
     # we exploit the fact that find_by_id(nil) returns nil
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,7 @@ class SessionsController < ApplicationController
   skip_before_filter :set_current_user
   def create
     auth = request.env["omniauth.auth"]
-    user = Moviegoer.find_by_provider_and_uid(auth["provider"], auth["uid"]) ||
-    Moviegoer.create_with_omniauth(auth)
+    user = Moviegoer.find_by_provider_and_uid(auth["provider"], auth["uid"]) || Moviegoer.create_with_omniauth(auth)
     session[:user_id] = user.id
     redirect_to movies_path
   end
